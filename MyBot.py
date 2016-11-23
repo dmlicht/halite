f598b5f0ee0735bc8aa15dcb6bc2f5afaba1021b
David Lichtenberg <david.m.lichtenberg@gmail.com>
2016-11-22 21:21:18 -0500

    cells will attack nearest neighbors

--- MyBot.py
+++ MyBot.py
@@ -1,8 +1,53 @@
 from hlt import *
 from networking import *
+import logging
 
+logger = logging.getLogger('botlogger')
 myID, gameMap = getInit()
 sendInit("MyPythonBot")
+
+
+def choose_move(location):
+    site = gameMap.getSite(location)
+    # wait to build up some strength
+    if site.strength < 10:
+        return Move(location, STILL)
+
+    neigh = neighbors(location)
+    enemies = are_enemies(neigh)
+
+    # Our space is on the border. Attack the weakest enemy.
+    if len(enemies) > 0:
+        weakest_direction, _ = weakest(enemies)
+        return Move(location, weakest_direction)
+
+    # Move inner
+    else:
+        return Move(location, random.choice([NORTH, EAST]))
+
+
+def neighbors(location):
+    """Return all locations touching a given location"""
+    return {direction: gameMap.getSite(location, direction) for direction in DIRECTIONS[1:]}
+
+
+def are_enemies(direction_sites):
+    """Filter for sites that are controlled by enemies"""
+    return {direction: site for (direction, site) in direction_sites.items() if site.owner != myID}
+
+
+def weakest(direction_sites):
+    """Return the weakest enemy"""
+    weakest_strength = 999
+    weakest_direction = None
+    weakest_site = None
+    for direction, site in direction_sites.items():
+        if site.strength < weakest_strength:
+            weakest_strength = site.strength
+            weakest_direction = direction
+            weakest_site = site
+    return weakest_direction, weakest_site
+
 
 while True:
     moves = []
@@ -12,5 +57,5 @@ while True:
             location = Location(x, y)
             site = gameMap.getSite(location)
             if site.owner == myID:
-                moves.append(Move(location, random.choice(DIRECTIONS)))
+                moves.append(choose_move(location))
     sendFrame(moves)
